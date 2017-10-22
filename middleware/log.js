ad22d377952ac5123610c0f8c90cd96ea182b625
Dxy <denight@qq.com>
2017-10-22 16:00:46 +0800

    更新代理服务

--- middleware/log.js
+++ middleware/log.js
@@ -1,7 +1,8 @@
+const moment = require('moment')
+
 module.exports = function (ctx) {
   let clientIP = ctx.req.connection.remoteAddress
-  let referer = ctx.req.headers.referer
-  let now = Date()
-
-  console.log(now, clientIP, '=>', referer)
+  let referer = ctx.req.headers.referer || ctx.req.headers.host
+  let now = moment().format('YYYY/MM/DD HH:MM:SS')
+  console.log('%s : %s visited %s', now, clientIP.slice(7), referer)
 }
